ed3a5d78135b4c2f0c1bfc2f05c8bfcea59d5a88
jvalvarez <jvalvarez@ccss.sa.cr>
2025-11-15 19:54:57 -0600

    Actualizacion Se actulizan variables para que sirva para producción

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDExiYxl4E0Y-CkgqlR7_MVQeYNcaJu9P8",
+    authDomain: "control-transportes-prod.firebaseapp.com",
+    projectId: "control-transportes-prod",
+    storageBucket: "control-transportes-prod.firebasestorage.app",
+    messagingSenderId: "333248052710",
+    appId: "1:333248052710:web:cfcc66feef92ec1dbb6a27",
+    measurementId: "G-MBKLDJ0X8Q"
+  }
 };
