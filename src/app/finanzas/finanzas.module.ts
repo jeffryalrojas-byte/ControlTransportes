b8b2b099605957551c9d4cbdbefc3642c9345746
jvalvarez <jvalvarez@ccss.sa.cr>
2026-08-14 10:55:31 -0600

    Se modifica el diseño de Finanzas

--- src/app/finanzas/finanzas.module.ts
+++ src/app/finanzas/finanzas.module.ts
@@ -3,6 +3,14 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { FinanzasComponent } from './finanzas.component';
 import { RouterModule } from '@angular/router';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatTooltipModule } from '@angular/material/tooltip';
 
 @NgModule({
   declarations: [FinanzasComponent],
@@ -11,8 +19,15 @@ import { RouterModule } from '@angular/router';
     FormsModule,
     RouterModule.forChild([
       { path: '', component: FinanzasComponent }
-    ])
+    ]),
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatButtonModule,
+    MatIconModule,
+    MatChipsModule,
+    MatTooltipModule
   ]
 })
 export class FinanzasModule { }
-
